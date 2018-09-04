21e1b7ff472c060f354c50eae13cca8315fd37c1
luominmin <102625110@hdjt.hdad.local>
2018-09-04 08:55:24 +0800

    fix example shopcart-saga

--- examples/shopcart-saga/src/actions/index.js
+++ examples/shopcart-saga/src/actions/index.js
@@ -1,10 +1,12 @@
 import shop from '../api/shop'
 import * as types from '../constants/ActionTypes'
 
-export const receiveProducts = products => ({
-  type: types.RECEIVE_PRODUCTS,
-  products
-})
+export const receiveProducts = products => {
+  return {
+    type: types.RECEIVE_PRODUCTS,
+    products
+  }
+}
 
 export const addToCart = productId => {
   return {
