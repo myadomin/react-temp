fec9421d340cac77c6a8797ff2601c77dfd5e0f4
luominmin <luominmin@hdsc.com>
2018-09-12 10:22:05 +0800

    fix bug: when input url, antd Menu selectedKeys is wrong

--- src/main.js
+++ src/main.js
@@ -5,13 +5,16 @@ import ReactDOM from 'react-dom'
 import App from '@src/view/app'
 import '@src/style/base.css'
 import { Provider } from 'react-redux'
+import { HashRouter } from 'react-router-dom'
 import store from '@src/store/store'
 // 打开 mockjs拦截请求，返回mock数据
 // 关闭 请求服务器数据
 import mock from '@src/mock'
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </Provider>,
   document.getElementById('root')
 )
--- src/view/app/index.js
+++ src/view/app/index.js
@@ -1,97 +1,93 @@
 import React, { Component } from 'react'
 import { Layout, Menu, Icon } from 'antd'
-import { HashRouter, Route, Link } from 'react-router-dom'
+import { Route, Link, NavLink, withRouter } from 'react-router-dom'
 import s from './index.css'
 import GlobalHeader from './GlobalHeader'
 import Home from '@src/view/home'
 import Todos from '@src/view/todos/todos'
 import Shopcart from '@src/view/shopcart/shopcart'
 
-export default class App extends Component {
+class App extends Component {
   constructor (props, context) {
     super(props)
     this.state = {
-      current: 'home'
+      current: ''
     }
-  }
-
-  componentWillMount () {
-    const current = window.location.hash.replace(/#\//, '')
-    this.setState({
-      current
-    })
   }
 
   render () {
     const { Header, Content, Footer, Sider } = Layout
     const { SubMenu } = Menu
     const imgUrl = require('@src/assets/logo.svg')
+    // withRouter(App)以后 this.props就有location等路由相关信息了
+    // 每次刷新 切换导航 重新输入url等都会进入这里 重新算出current给到selectedKeys
+    const current = this.props.location.pathname.replace(/\//, '') || 'home'
     return (
-      <HashRouter>
-        <Layout style={{ height: '100vh' }}>
-          <Sider
-            breakpoint="lg"
-            collapsedWidth="0"
-            // onBreakpoint={(broken) => { console.log(broken) }}
-            // onCollapse={(collapsed, type) => { console.log(collapsed, type) }}
+      <Layout style={{ height: '100vh' }}>
+        <Sider
+          breakpoint="lg"
+          collapsedWidth="0"
+          // onBreakpoint={(broken) => { console.log(broken) }}
+          // onCollapse={(collapsed, type) => { console.log(collapsed, type) }}
+        >
+          <div className={s.logo}>
+            <img src={imgUrl} className={s.logoSvg}></img>
+            <h1 className={s.h1}>啊啊啊啊附带</h1>
+          </div>
+          <Menu theme="dark" mode="inline"
+            selectedKeys={[current]}
+            defaultOpenKeys={['1']}
           >
-            <div className={s.logo}>
-              <img src={imgUrl} className={s.logoSvg}></img>
-              <h1 className={s.h1}>啊啊啊啊附带</h1>
+            <SubMenu key="1" title={<span><Icon type="appstore" /><span>Examples</span></span>}>
+              <Menu.Item key="home">
+                <NavLink to="/home">
+                  <span className="nav-text">home</span>
+                </NavLink>
+              </Menu.Item>
+              <Menu.Item key="todos">
+                <NavLink to="/todos">
+                  <span className="nav-text">todos</span>
+                </NavLink>
+              </Menu.Item>
+              <Menu.Item key="shopcart">
+                <NavLink to="/shopcart">
+                  <span className="nav-text">shopcart</span>
+                </NavLink>
+              </Menu.Item>
+            </SubMenu>
+            <Menu.Item key="2">
+              <Icon type="desktop" />
+              <span className="nav-text">斯芬斯蒂芬</span>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <Icon type="inbox" />
+              <span className="nav-text">方方法分</span>
+            </Menu.Item>
+            <Menu.Item key="4">
+              <Icon type="user" />
+              <span className="nav-text">撒旦法</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{ background: '#fff', padding: 0 }}>
+            <GlobalHeader />
+          </Header>
+          <Content style={{ margin: '24px 16px 0' }}>
+            <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+              <Route exact path="/" component={Home} />
+              <Route path="/home" component={Home} />
+              <Route path="/todos" component={Todos} />
+              <Route path="/shopcart" component={Shopcart} />
             </div>
-            <Menu theme="dark" mode="inline"
-              defaultSelectedKeys={[this.state.current]}
-              defaultOpenKeys={['1']}
-            >
-              <SubMenu key="1" title={<span><Icon type="appstore" /><span>Examples</span></span>}>
-                <Menu.Item key="home">
-                  <Link to="/home" replace>
-                    <span className="nav-text">home</span>
-                  </Link>
-                </Menu.Item>
-                <Menu.Item key="todos">
-                  <Link to="/todos" replace>
-                    <span className="nav-text">todos</span>
-                  </Link>
-                </Menu.Item>
-                <Menu.Item key="shopcart">
-                  <Link to="/shopcart" replace>
-                    <span className="nav-text">shopcart</span>
-                  </Link>
-                </Menu.Item>
-              </SubMenu>
-              <Menu.Item key="2">
-                <Icon type="desktop" />
-                <span className="nav-text">斯芬斯蒂芬</span>
-              </Menu.Item>
-              <Menu.Item key="3">
-                <Icon type="inbox" />
-                <span className="nav-text">方方法分</span>
-              </Menu.Item>
-              <Menu.Item key="4">
-                <Icon type="user" />
-                <span className="nav-text">撒旦法</span>
-              </Menu.Item>
-            </Menu>
-          </Sider>
-          <Layout>
-            <Header style={{ background: '#fff', padding: 0 }}>
-              <GlobalHeader />
-            </Header>
-            <Content style={{ margin: '24px 16px 0' }}>
-              <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
-                <Route exact path="/" activeStyle={{color: 'red'}} component={Home} />
-                <Route path="/home" activeStyle={{color: 'red'}} component={Home} />
-                <Route path="/todos" activeStyle={{color: 'red'}} component={Todos} />
-                <Route path="/shopcart" activeStyle={{color: 'red'}} component={Shopcart} />
-              </div>
-            </Content>
-            <Footer style={{ textAlign: 'center' }}>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>
               xxxx ©2018 Created by xxxxx
-            </Footer>
-          </Layout>
+          </Footer>
         </Layout>
-      </HashRouter>
+      </Layout>
     )
   }
 }
+
+export default withRouter(App)
