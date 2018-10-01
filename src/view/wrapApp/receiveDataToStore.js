8f72bf7d91e683efe23c78e73866b0dbb4388b48
adomin <adomin@163.com>
2018-10-01 12:17:57 +0800

    封装sendWsMsg receiveDataToStore1

---
+++ src/view/wrapApp/receiveDataToStore.js
@@ -0,0 +1,10 @@
+export const receiveDataToStore = (json, wsStore) => {
+  switch (json.rpcId) {
+    case 'checkIsConnection':
+      return console.log(json.data)
+    case 'addMessage':
+      return wsStore.messageList.push(json.data)
+    default:
+      console.log('客户端：没有找到此消息对应的rpcId')
+  }
+}
